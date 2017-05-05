5e8bcb9233bf306862f638d338f0312ba41798f1
jBarz <jbarboza@ca.ibm.com>
2017-05-04 22:32:22 -0400

    use independent openssl client on zos

--- test/common.js
+++ test/common.js
@@ -160,6 +160,8 @@ Object.defineProperty(exports, 'opensslCli', {get: function() {
   if (process.config.variables.node_shared_openssl) {
     // use external command
     opensslCli = 'openssl';
+  } else if (exports.isZos) {
+    opensslCli = 'openssl';
   } else {
     // use command built from sources included in Node.js repository
     opensslCli = path.join(path.dirname(process.execPath), 'openssl-cli');
