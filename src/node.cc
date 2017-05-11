85410d7f7fd11907a8318800264a14dcb48b0722
jBarz <jbarboza@ca.ibm.com>
2017-05-11 18:57:36 -0400

    fix encoding for dependency versions

--- src/node.cc
+++ src/node.cc
@@ -3328,10 +3328,10 @@ void SetupProcessObject(Environment* env,
                     OneByteString(env->isolate(), *E2A(uv_version_string())));
   READONLY_PROPERTY(versions,
                     "\x7a\x6c\x69\x62",
-                    FIXED_ONE_BYTE_STRING(env->isolate(), ZLIB_VERSION));
+                    FIXED_ONE_BYTE_STRING(env->isolate(), NODE_STRINGIFY(ZLIB_VERSION)));
   READONLY_PROPERTY(versions,
                     "\x61\x72\x65\x73",
-                    FIXED_ONE_BYTE_STRING(env->isolate(), ARES_VERSION_STR));
+                    FIXED_ONE_BYTE_STRING(env->isolate(), NODE_STRINGIFY(ARES_VERSION_STR)));
 
 #if defined(NODE_HAVE_I18N_SUPPORT) && defined(U_ICU_VERSION)
   READONLY_PROPERTY(versions,
