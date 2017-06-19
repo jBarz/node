30a68b049a9ccfe202f9543bfe1497384acae79e
Anisha Rohra <Anisha.Rohra@ibm.com>
2017-06-19 09:48:00 -0400

    working on optimizing conversion script with regex

--- tools/conversion_tool/eb2as.sh
+++ tools/conversion_tool/eb2as.sh
@@ -1,46 +1,52 @@
 #!/sh
 
-curr=0
-file=0
-count=0
+# curr=0
+# file=0
+# count=0
 
 # loop to find and remove any potential propositional arguments that could
 # prevent the pre-processing step from producing a .u header file
-for var in $@
-do
-    match=$(echo $var | grep "\-c")
-    match2=$(echo $var | grep "\-MF")
-    if [[ ! -z $match ]]
-    then
-        deleted[count]=$var
-        count=$((count+1))
-        continue
-    elif [[ ! -z $match2 ]]
-    then
-        file=1
-        deleted[count]=$var
-        count=$((count+1))
-        continue
-    else
-        if [ $file = 0 ]
-        then
-            new[curr]=$var
-            curr=$((curr+1))
-        else
-            file=0
-            deleted[count]=$var
-            count=$((count+1))
-        fi
-    fi
-done
+# for var in $@
+# do
+#     match=$(echo $var | grep "\-c")
+#     match2=$(echo $var | grep "\-MF")
+#     if [[ ! -z $match ]]
+#     then
+#         deleted[count]=$var
+#         count=$((count+1))
+#         continue
+#     elif [[ ! -z $match2 ]]
+#     then
+#         file=1
+#         deleted[count]=$var
+#         count=$((count+1))
+#         continue
+#     else
+#         if [ $file = 0 ]
+#         then
+#             new[curr]=$var
+#             curr=$((curr+1))
+#         else
+#             file=0
+#             deleted[count]=$var
+#             count=$((count+1))
+#         fi
+#     fi
+# done
 
-#iterate over each .c, .cc, .cpp file that's been given and call the convereter
+first=$(echo $@ | sed -E 's/(.+)-c (.+)/\1\2/')
+deleted[0]="-c"
+new=$(echo $first | sed -E 's/(.+)-MF [A-Z0-9a-z/\._-]*(.+)/\1\2/')
+deleted[1]=$(echo $first | sed -E 's/.+(-MF [A-Z0-9a-z/\._-]*).+/\1/')
+echo $new
+
+# iterate over each .c, .cc, .cpp file that's been given and call the convereter
 CFLAG=0
 count=0
 compiled=0
-for var in ${new[*]}
+for var in $new
 do
-    fileend=$(echo $var | sed -E 's/.+(\.[a-z_]+)/\1/')
+    fileend=$(echo $var | sed -E 's/.+(\.[A-Z0-9a-z_]+)/\1/')
     if [ $fileend = ".cc" ] || [ $fileend = ".cpp" ] || [ $fileend = ".c" ] || [ $fileend = ".cxx" ]
     then
         if [ $compiled = 0 ]
@@ -49,13 +55,13 @@ do
             then
                 compiled=1
                 CFLAG=1
-                xlclang -E ${new[*]} > garbage.c
+                njsc -E $new > garbage.c
             else
                 compiled=1
-                xlclang++ -E ${new[*]} > garbage.c
+                njsc++ -E $new > garbage.c
             fi
         fi
-        HEADER=$(echo $var | sed -E 's/.*\/([a-z0-9_]+)\.[a-z]+/\1.u/')
+        HEADER=$(echo $var | sed -E 's/.*\/([a-zA-Z0-9_]+)\.[a-zA-Z]+/\1.u/')
         TEMP=$(echo $var | sed -E 's/(.+)\.([a-z]+)/\1_temp.\2/')
         python $(dirname ${CXX})/ebcdic2ascii.py -H $HEADER $var $TEMP
         COMPILE[count]=$TEMP
@@ -69,9 +75,9 @@ done
 # compile using the temp file that has been converted into ascii
 if [ $CFLAG = 1 ]
 then
-    xlclang ${COMPILE[*]} ${deleted[*]}
+    njsc ${COMPILE[*]} ${deleted[*]}
 else
-    xlclang++ ${COMPILE[*]} ${deleted[*]}
+    njsc++ ${COMPILE[*]} ${deleted[*]}
 fi
 
 # get rid of all files created
