c078bf1183cf547a3a89fb77577254d80278d857
jBarz <jbarboza@ca.ibm.com>
2017-08-08 18:28:07 -0400

    fix encoding in stack string within exception

--- src/node_contextify.cc
+++ src/node_contextify.cc
@@ -686,7 +686,7 @@ class ContextifyScript : public BaseObject {
 
     Local<String> decorated_stack = String::Concat(
         String::Concat(arrow.As<String>(),
-          FIXED_ONE_BYTE_STRING(env->isolate(), "\n")),
+          FIXED_ONE_BYTE_STRING(env->isolate(), u8"\n")),
         stack.As<String>());
     err_obj->Set(env->stack_string(), decorated_stack);
     err_obj->SetPrivate(
