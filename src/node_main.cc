0be6a395fd3c10f50a1ce8a402cbc9f11a8315cd
John Barboza <jbarboza@ca.ibm.com>
2018-05-15 11:31:37 -0700

    resolve symlinks when setting LIBPATH

--- src/node_main.cc
+++ src/node_main.cc
@@ -60,19 +60,29 @@ int wmain(int argc, wchar_t *wargv[]) {
 #include <stdlib.h>
 
 void setlibpath(void) {
+  std::vector<char> argv(512, 0);
   std::vector<char> parent(512, 0);
   W_PSPROC buf;
   int token = 0;
   pid_t mypid = getpid();
   memset(&buf, 0, sizeof(buf));
-  buf.ps_pathlen = parent.size();
-  buf.ps_pathptr = &parent[0];
+  buf.ps_pathlen = argv.size();
+  buf.ps_pathptr = &argv[0];
   while ((token = w_getpsent(token, &buf, sizeof(buf))) > 0) {
     if (buf.ps_pid == mypid) {
+      /* Found our process. */
+
+      /* Resolve path to find true location of executable. */
+      if (realpath(&argv[0], &parent[0]) == NULL)
+        break;
+
+      /* Get parent directory. */
       dirname(&parent[0]);
+      /* Get parent's parent directory. */
       std::vector<char> parent2(parent.begin(), parent.end());
       dirname(&parent2[0]);
 
+      /* Append new paths to libpath. */
       std::ostringstream libpath;
       libpath << getenv("LIBPATH");
       libpath << ":" << &parent[0] << "/obj.target/";
